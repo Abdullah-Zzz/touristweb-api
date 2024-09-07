0996485ec448e4520116ecdd6c53cf49dcc8ae40
abdullah-zzz <ak0344494@gmail.com>
2024-09-07 21:49:09 +0500

    3 commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 
-const DBurl= process.env.MONGO_URL_LOCAL
+const DBurl= process.env.MONGO_URL
 
 const connectDB = mongoose.connect(DBurl)
                   .then(() =>{
