8a5145c96449d4067f47119272cb6f1391585192
abdullah-zzz <ak0344494@gmail.com>
2025-08-03 07:11:06 +0500

    comment changes

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -8,20 +8,17 @@ const addComment = async (req, res) => {
         const { comment, rating } = req.body
         if (!comment) return res.status(404).json('No comment Found');
         const today = new Date();
-        const yyyy = today.getFullYear();
-        let mm = today.getMonth() + 1; 
-        let dd = today.getDate();
         const userID = req.id
         const userDetails = await userModel.findById(userID)
         const chkTripExits = await tripSchema.findOne({ "mainData.packages.id": id })
         if (chkTripExits) {
             const chkComments = await schema.findOne({ id })
             if (chkComments) {
-                await schema.findByIdAndUpdate(chkComments._id, { $push: { comments: [{ comment: comment, createdBy: userDetails.name, createdAt: `${dd}/${mm}/${yyyy}`, rating: rating }] } }, { new: true })
+                await schema.findByIdAndUpdate(chkComments._id, { $push: { comments: [{ comment: comment, createdBy: userDetails.name, createdAt: new Date(), rating: rating }] } }, { new: true })
                 res.status(200).json("Comment Added")
             }
             else {
-                const newComment = new schema({ id, comments: [{ comment: comment, createdBy: userDetails.name, createdAt: `${dd}/${mm}/${yyyy}`, rating: rating }] })
+                const newComment = new schema({ id, comments: [{ comment: comment, createdBy: userDetails.name, createdAt: new Date(), rating: rating }] })
                 await newComment.validate()
                 await newComment.save()
                 return res.status(200).json("Comment Added")
@@ -41,12 +38,15 @@ const addComment = async (req, res) => {
 
 const getComments = async (req, res) => {
     const id = req.params.id
-    const chkComments = await schema.findOne({ id })
-    if (chkComments) {
-        res.status(200).json(chkComments)
+    const chkComments = await schema.find({ id })
+    if (chkComments.length > 0) {
+        chkComments[0].comments.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        return res.status(200).json(chkComments[0])
     }
     else {
-        res.status(404).json('No comments')
+        return res.status(404).json('No comments')
     }
 }
 
